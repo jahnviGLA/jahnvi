1943490163bba1801f7a59fc12e13f0b9c141eae
Ravi Goswami <zneroravi@gmail.com>
2023-10-26 15:18:19 +0530

    php

---
+++ data1.php
@@ -0,0 +1,7 @@
+{ "records":[ 
+	{"Name":"Ravi","City":"Mathura","Marks":"86"}, 
+	{"Name":"Udipto","City":"Delhi","Marks":"97"}, 
+	{"Name":"Harshit","City":"Agra","Marks":"67"}, 
+	{"Name":"Aman","City":"Kanpur","Marks":"86"}, 
+	{"Name":"Rupali","City":"Amritsar","Marks":"86"}, 
+] }
