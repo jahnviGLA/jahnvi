9105b959086220719b5568191f05c0e74a469c9d
Ravi Goswami <zneroravi@gmail.com>
2023-10-26 15:20:33 +0530

    Update data1.php

--- data1.php
+++ data1.php
@@ -3,5 +3,5 @@
 	{"Name":"Udipto","City":"Delhi","Marks":"97"}, 
 	{"Name":"Harshit","City":"Agra","Marks":"67"}, 
 	{"Name":"Aman","City":"Kanpur","Marks":"86"}, 
-	{"Name":"Rupali","City":"Amritsar","Marks":"86"}, 
+	{"Name":"Rupali","City":"Amritsar","Marks":"86"}
 ] }
